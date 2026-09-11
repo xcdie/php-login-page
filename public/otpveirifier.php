77ecb9e5f4d4ecbaff8d07a4e1bf61cdcf589a14
jamlick sagide <jamlickmarsh400@gmail.com>
2026-09-11 03:13:05 -0400

    Update

--- public/otpveirifier.php
+++ public/otpveirifier.php
@@ -1,1 +1,7 @@
-
+session start();
+require mailer.php;
+require db.php;
+if ($_SERVER['REQUEST_METHOD']=== '$_POST'){
+$email= trim($_POST['email'] ?? '');
+$password= ($_POST['password'] ?? '');
+}
